6a4145e223f2365ce2478f00ca217641ce83503e
Grigor Jivkov Marinov <grigormarinov761@gmail.com>
2025-10-21 17:44:26 +0300

    Implemented error boundary

--- src/HotelsManagementSystem.Client/src/App.jsx
+++ src/HotelsManagementSystem.Client/src/App.jsx
@@ -5,22 +5,25 @@ import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 import Register from "./components/Auth/Register/Register";
 import Login from "./components/Auth/Login/Login";
+import ErrorBoundary from "./components/Error-Boundary/Error-Boundary";
 
 function App() {
   return (
-    <div className="app">
-      <BrowserRouter>
-        <Navigation />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </main>
-        <Footer />
-      </BrowserRouter>
-    </div>
+    <ErrorBoundary>
+      <div className="app">
+        <BrowserRouter>
+          <Navigation />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </main>
+          <Footer />
+        </BrowserRouter>
+      </div>
+    </ErrorBoundary>
   );
 }
 
