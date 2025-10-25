b0fe49de1cef208e001b8e66d6f61f69786b6595
Grigor Jivkov Marinov <grigormarinov761@gmail.com>
2025-10-25 16:50:52 +0300

    Added Email Provider service

---
+++ src/HotelsManagementSystem.Api/HotelsManagementSystem.Api/Services/EmailProvider/EmailProvider.cs
@@ -0,0 +1,26 @@
+﻿
+using Smtp2Go.Api;
+using Smtp2Go.Api.Models.Emails;
+
+namespace HotelsManagementSystem.Api.Services.EmailProvider
+{
+    public class EmailProvider : IEmailProvider
+    {
+        private readonly Smtp2GoApiService _smtp2GoApiService;
+
+        public EmailProvider(Smtp2GoApiService smtp2GoApiService)
+        {
+            _smtp2GoApiService = smtp2GoApiService;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string body, string sender)
+        {
+            var message = new EmailMessage(sender, toEmail);
+
+            message.Subject = subject;
+            message.BodyHtml = body;
+
+            var response = await _smtp2GoApiService.SendEmail(message);
+        }
+    }
+}
