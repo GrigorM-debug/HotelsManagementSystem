5c9f1705dc59e860effe5fd54b453c2fba780dbf
Grigor Jivkov Marinov <grigormarinov761@gmail.com>
2025-10-28 22:03:39 +0200

    Added CreateHotelDto

---
+++ src/HotelsManagementSystem.Api/HotelsManagementSystem.Api/DTOs/Admin/Hotels/CreateHotelDto.cs
@@ -0,0 +1,17 @@
+﻿namespace HotelsManagementSystem.Api.DTOs.Admin.Hotels
+{
+    public class CreateHotelDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public int Stars { get; set; }
+        public TimeSpan CheckInTime { get; set; }
+        public TimeSpan CheckOutTime { get; set; }
+        public List<Guid> AmenityIds { get; set; } = new List<Guid>();
+    }
+}
