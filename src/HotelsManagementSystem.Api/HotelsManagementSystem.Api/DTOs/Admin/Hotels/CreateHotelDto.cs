3d74de83da1c11e7c5752bdb7601ef6834a61ef0
Grigor Jivkov Marinov <grigormarinov761@gmail.com>
2025-10-29 20:26:31 +0200

    Added the data annotations attributes for validating the CreateHotelDto

--- src/HotelsManagementSystem.Api/HotelsManagementSystem.Api/DTOs/Admin/Hotels/CreateHotelDto.cs
+++ src/HotelsManagementSystem.Api/HotelsManagementSystem.Api/DTOs/Admin/Hotels/CreateHotelDto.cs
@@ -1,17 +1,46 @@
-﻿namespace HotelsManagementSystem.Api.DTOs.Admin.Hotels
+﻿using HotelsManagementSystem.Api.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelsManagementSystem.Api.DTOs.Admin.Hotels
 {
     public class CreateHotelDto
     {
+        [Required(ErrorMessage = GeneralConstants.ValueRequiredErrorMessage)]
+        [StringLength(HotelConstants.NameMaxLength,
+            MinimumLength = HotelConstants.NameMinLength, ErrorMessage = GeneralConstants.ValueLengthErrorMessage)]
+        [RegularExpression(HotelConstants.NameRegexPattern, ErrorMessage = GeneralConstants.ValueInvalidPatternErrorMessage)]
         public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = GeneralConstants.ValueRequiredErrorMessage)]
+        [StringLength(HotelConstants.DescriptionMaxLength,
+            MinimumLength = HotelConstants.DescriptionMinLength, ErrorMessage = GeneralConstants.ValueLengthErrorMessage)]
         public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = GeneralConstants.ValueRequiredErrorMessage)]
+        [StringLength(GeneralConstants.AddressMaxLength,
+            MinimumLength = GeneralConstants.AddressMinLength, ErrorMessage = GeneralConstants.ValueLengthErrorMessage)]
+        [RegularExpression(GeneralConstants.AddressRegexPattern, ErrorMessage = GeneralConstants.ValueInvalidPatternErrorMessage)]
         public string Address { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = GeneralConstants.ValueRequiredErrorMessage)]
+        [StringLength(GeneralConstants.CityMaxLength,
+            MinimumLength = GeneralConstants.CityMinLength, ErrorMessage = GeneralConstants.ValueLengthErrorMessage)]
+        [RegularExpression(GeneralConstants.CityRegexPattern, ErrorMessage = GeneralConstants.ValueInvalidPatternErrorMessage)]
         public string City { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = GeneralConstants.ValueRequiredErrorMessage)]
+        [StringLength(GeneralConstants.CountryMaxLength,
+            MinimumLength = GeneralConstants.CountryMinLength, ErrorMessage = GeneralConstants.ValueLengthErrorMessage)]
+        [RegularExpression(GeneralConstants.CountryRegexPattern, ErrorMessage = GeneralConstants.ValueInvalidPatternErrorMessage)]
         public string Country { get; set; } = string.Empty;
-        public double? Latitude { get; set; }
-        public double? Longitude { get; set; }
+        
+        [Range(HotelConstants.StarsMinValue, HotelConstants.StarsMaxValue, ErrorMessage = GeneralConstants.ValueRangeErrorMessage)]
         public int Stars { get; set; }
+
         public TimeSpan CheckInTime { get; set; }
+
         public TimeSpan CheckOutTime { get; set; }
+
         public List<Guid> AmenityIds { get; set; } = new List<Guid>();
     }
 }
