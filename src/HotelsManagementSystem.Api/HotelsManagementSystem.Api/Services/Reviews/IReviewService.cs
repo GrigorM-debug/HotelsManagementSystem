cec49351a225d7d052b3e45fb36c260804c612ae
Grigor Jivkov Marinov <grigormarinov761@gmail.com>
2025-11-25 16:43:31 +0200

    Implemented the backend logic for creating reviews

---
+++ src/HotelsManagementSystem.Api/HotelsManagementSystem.Api/Services/Reviews/IReviewService.cs
@@ -0,0 +1,11 @@
+﻿using HotelsManagementSystem.Api.DTOs.Reviews;
+
+namespace HotelsManagementSystem.Api.Services.Reviews
+{
+    public interface IReviewService
+    {
+        public Task<bool> ReviewAlreadyExistsByCustomerIdAndHotelIdAsync(Guid customerId,  Guid hotelId);
+
+        public Task<bool> CreateReviewAsync(Guid customerId, Guid hotelId, CreateReviewDto inputDto);
+    }
+}
---
+++ src/HotelsManagementSystem.Api/HotelsManagementSystem.Api/Services/Reviews/ReviewService.cs
@@ -0,0 +1,46 @@
+﻿using HotelsManagementSystem.Api.Data;
+using HotelsManagementSystem.Api.Data.Models.Reviews;
+using HotelsManagementSystem.Api.DTOs.Reviews;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelsManagementSystem.Api.Services.Reviews
+{
+    public class ReviewService : IReviewService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReviewService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> CreateReviewAsync(Guid customerId, Guid hotelId, CreateReviewDto inputDto)
+        {
+            var newReview = new Review()
+            {
+                Id = Guid.NewGuid(),
+                CustomerId = customerId,
+                HotelId = hotelId,
+                Rating = inputDto.Rating,
+                Comment = inputDto.Comment,
+                CreatedOn = DateTime.UtcNow,
+            };
+
+            await _context.Reviews.AddAsync(newReview);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> ReviewAlreadyExistsByCustomerIdAndHotelIdAsync(Guid customerId, Guid hotelId)
+        {
+            var reviewExists = await _context
+                .Reviews
+                .AnyAsync(r => 
+                    r.HotelId == hotelId && 
+                    r.CustomerId == customerId && 
+                    !r.IsDeleted);
+
+            return reviewExists;
+        }
+    }
+}
